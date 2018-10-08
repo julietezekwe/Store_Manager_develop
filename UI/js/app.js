a12f836b7a15ce7607c4bb4da283ab206d41d0ea
julietezekwe <julietezekwe@gmail.com>
2018-10-08 15:59:26 +0100

    fixes js bug

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,12 +1,12 @@
-const scripts =  [
-    "js/components/header.js", 
-    "js/components/card.js", 
-    "js/components/footer.js", 
-];
-scripts.forEach(script => {
-    let imported = document.createElement('script');
-    imported.src = document.location.hostname + script;
-    document.head.appendChild(imported);
-});
+// const scripts =  [
+//     "js/components/header.js", 
+//     "js/components/card.js", 
+//     "js/components/footer.js", 
+// ];
+// scripts.forEach(script => {
+//     let imported = document.createElement('script');
+//     imported.src = script;
+//     document.head.appendChild(imported);
+// });
 
 
